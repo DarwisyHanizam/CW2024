42f659924ed9e6e00dfb4f0a2d33497f7d5ca70a
DarwisyHanizam <darwisyhanizam@gmail.com>
2024-12-13 10:38:22 +0800

    Fixed user movement edge case

--- src/main/java/com/example/demo/levels/handler/LevelInitializer.java
+++ src/main/java/com/example/demo/levels/handler/LevelInitializer.java
@@ -4,14 +4,17 @@ import javafx.animation.KeyFrame;
 import javafx.animation.Timeline;
 import javafx.scene.Scene;
 import javafx.scene.input.KeyCode;
-import javafx.scene.input.KeyEvent;
-import javafx.event.EventHandler;
 import javafx.util.Duration;
 
 public class LevelInitializer{
 
 	private LevelAll levelAll;
 	private LevelAbstract levelAbstract;
+
+	private boolean movingLeft, movingRight, movingUp, movingDown;
+	private boolean firedShot;
+	private long timeLeftPress, timeRightPress, timeUpPress, timeDownPress = 0;
+	private static long DELAY = 100;
 
 	public LevelInitializer(LevelAll levelAll, LevelAbstract levelAbstract){
 		this.levelAll = levelAll;
@@ -35,23 +38,97 @@ public class LevelInitializer{
 		levelAll.background.setFocusTraversable(true);
 		levelAll.background.setFitHeight(levelAll.screenHeight);
 		levelAll.background.setFitWidth(levelAll.screenWidth);
-		levelAll.background.setOnKeyPressed(new EventHandler<KeyEvent>() {
-			public void handle(KeyEvent e) {
-				KeyCode kc = e.getCode();
-				if (kc == KeyCode.UP) levelAll.user.moveUp();
-				if (kc == KeyCode.DOWN) levelAll.user.moveDown();
-				if (kc == KeyCode.LEFT) levelAll.user.moveLeft();
-				if (kc == KeyCode.RIGHT) levelAll.user.moveRight();
-				if (kc == KeyCode.SPACE) levelAll.levelMechanics.fireProjectile();
-			}
-		});
-		levelAll.background.setOnKeyReleased(new EventHandler<KeyEvent>() {
-			public void handle(KeyEvent e) {
-				KeyCode kc = e.getCode();
-				if (kc == KeyCode.UP || kc == KeyCode.DOWN) levelAll.user.stop("Vertical");
-				if (kc == KeyCode.LEFT || kc == KeyCode.RIGHT) levelAll.user.stop("Horizontal");
-			}
-		});
+		updateUserInput();
 		levelAll.root.getChildren().add(levelAll.background);
+	}
+
+	private void updateUserInput() {
+		levelAll.background.setOnKeyPressed(e -> {
+			switch (e.getCode()) {
+				case KeyCode.LEFT:
+					movingLeft = true;
+					timeLeftPress = System.currentTimeMillis();
+					break;
+				case KeyCode.RIGHT:
+					movingRight = true;
+					timeRightPress = System.currentTimeMillis();
+					break;
+				case KeyCode.UP:
+					movingUp = true;
+					timeUpPress = System.currentTimeMillis();
+					break;
+				case KeyCode.DOWN:
+					movingDown = true;
+					timeDownPress = System.currentTimeMillis();
+					break;
+				default:
+					break;
+			}
+			updateMovement();
+		});
+
+		levelAll.background.setOnKeyReleased(e -> {
+			switch (e.getCode()) {
+				case KeyCode.LEFT:
+					movingLeft = false;
+					timeLeftPress = System.currentTimeMillis() + DELAY;
+					break;
+				case KeyCode.RIGHT:
+					movingRight = false;
+					timeRightPress = System.currentTimeMillis() + DELAY;;
+					break;
+				case KeyCode.UP:
+					movingUp = false;
+					timeUpPress = System.currentTimeMillis() + DELAY;;
+					break;
+				case KeyCode.DOWN:
+					movingDown = false;
+					timeDownPress = System.currentTimeMillis() + DELAY;;
+					break;
+				case KeyCode.SPACE:
+					firedShot = false;
+					break;
+				default:
+					break;
+			}
+			updateMovement();
+		});
+	}
+
+	private void updateMovement(){
+		if (movingLeft && movingRight) {
+			if (timeLeftPress > timeRightPress) {
+				levelAll.user.moveLeft();
+			} else {
+				levelAll.user.moveRight();
+			}
+		} else {
+			if (movingLeft) levelAll.user.moveLeft();
+			if (movingRight) levelAll.user.moveRight();
+		}
+
+		if (movingUp && movingDown) {
+			if (timeUpPress > timeDownPress) {
+				levelAll.user.moveUp();
+			} else {
+				levelAll.user.moveDown();
+			}
+		} else {
+			if (movingUp) levelAll.user.moveUp();
+			if (movingDown) levelAll.user.moveDown();
+		}
+
+		if (!movingLeft && !movingRight) {
+			levelAll.user.stop("Horizontal");
+		}
+
+		if (!movingUp && !movingDown) {
+			levelAll.user.stop("Vertical");
+		}
+
+		if (!firedShot) {
+			levelAll.levelMechanics.fireProjectile();
+			firedShot = true;
+		}
 	}
 }
